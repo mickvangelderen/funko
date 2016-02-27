dd55a8601832d4c137ab90248406459c9f07a457
Mick van Gelderen <mickvangelderen@gmail.com>
2016-02-27 10:59:44 +0100

    Use sort-object-circular for config sorting

--- tools/sort-configuration-files.js
+++ tools/sort-configuration-files.js
@@ -1,40 +1,13 @@
 import fs from 'fs'
 import Promise from 'bluebird'
+import sortObject from 'sort-object-circular'
 
 Promise.promisifyAll(fs)
 
 const ERROR_ON_CHANGES = process.argv.indexOf('--error-on-changes') !== -1
 
-const isArray = Array.isArray
-
-const isObject = value => Object.prototype.toString.call(value) === '[object Object]'
-
-function sortObjectKeys(input, inputs = [], outputs = []) {
-	// Immediately return values that do not have properties. 
-	if (!isArray(input) && !isObject(input)) return input
-
-	// Check if result already computed.
-	const index = inputs.indexOf(input)
-	if (index !== -1) return outputs[index]
-
-	// Create an output reference.
-	const output = isArray(input) ? [] :
-		Object.create(Object.getPrototypeOf(input))
-
-	// Store the input and output so they are used while recursively sorting nested objects. 
-	inputs.push(input)
-	outputs.push(output)
-
-	// Copy keys from input to output in a sorted manner. 
-	Object.keys(input).sort().forEach(key => {
-		output[key] = sortObjectKeys(input[key], inputs, outputs)
-	})
-
-	return output
-}
-
 function sortJson(string) {
-	return JSON.stringify(sortObjectKeys(JSON.parse(string)), null, 2) + '\n'
+	return JSON.stringify(sortObject(JSON.parse(string)), null, 2) + '\n'
 }
 
 function sortLines(string) {
